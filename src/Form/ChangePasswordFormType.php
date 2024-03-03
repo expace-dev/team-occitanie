5a72e1f5e6795b933a0110c1b351ab51434ee38f
Fredy <fredy6786@gmail.com>
2024-03-03 13:09:26 +0100

    refactorisation rezet password

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -3,50 +3,48 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
 
 class ChangePasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'options' => [
-                    'attr' => [
-                        'autocomplete' => 'new-password',
-                    ],
-                ],
-                'first_options' => [
-                    'constraints' => [
-                        new NotBlank([
-                            'message' => 'Please enter a password',
-                        ]),
-                        new Length([
-                            'min' => 6,
-                            'minMessage' => 'Your password should be at least {{ limit }} characters',
-                            // max length allowed by Symfony for security reasons
-                            'max' => 4096,
-                        ]),
-                    ],
-                    'label' => 'Mot de passe',
-                    'label_attr' => [
-                        'class' => 'col-lg-12'
-                    ],
-                ],
-                'second_options' => [
-                    'label' => 'Confirmation',
-                ],
-                'invalid_message' => 'The password fields must match.',
-                // Instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-            ])
+        ->add('plainPassword', PasswordType::class, [
+            'always_empty' => false,
+            'label' => 'Mot de passe*',
+            'constraints' => [
+                new Regex([
+                    'pattern' => '/[a-z]/',
+                    'message' => 'Au moins une lettre minuscule'
+                ]),
+                new Regex([
+                    'pattern' => '/[A-Z]/',
+                    'message' => 'Au moins une lettre majuscule'
+                ]),
+                new Regex([
+                    'pattern' => '/[1-9]/',
+                    'message' => 'Au moins un chiffre'
+                ]),
+                new Length([
+                    'min' => 14,
+                    'minMessage' => 'Au moins {{ limit }} caractères',
+                ]),
+                new NotCompromisedPassword(['message' => 'Ce mot de passe est compromis']),
+            ],
+            'attr' => [
+                'autocomplete' => 'off',
+                'class' => "gen"
+            ],
+            'mapped' => false
+        ])
         ;
     }
 
