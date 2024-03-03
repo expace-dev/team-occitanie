6db68726938987456e980af9b3cbe1e894d0cc49
Fredy <fredy6786@gmail.com>
2024-03-03 18:05:54 +0100

    update application

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -103,7 +103,7 @@ class ApplicationController extends AbstractController
             'formTache' => $formTache,
             'taches' => $tachesRepository->findBy(['statut' => true]),
             'formEvents' => $formEvents,
-            'evenements' => $evenements
+            'evenements' => $evenementsRepository->findBy(['dateEvents' >= new DateTime('now')])
         ]);
     }
     #[Route('/application/validation/tache/{id}', name: 'app_application_validation'), IsGranted('ROLE_USER')]
