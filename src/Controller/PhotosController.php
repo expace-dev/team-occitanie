ba106614288b963588ad17e09899ee987cfc1759
Fredy <fredy6786@gmail.com>
2024-03-14 11:21:09 +0100

    partage de photos

--- src/Controller/PhotosController.php
+++ src/Controller/PhotosController.php
@@ -137,7 +137,7 @@ class PhotosController extends AbstractController
                 'https://bot.team-occitanie.fr/post-photo/query/?username='.$photo->getUsers()->getUsername().'&avatar=https://www.team-occitanie.fr'.$photo->getUsers()->getAvatar().'&image=https://www.team-occitanie.fr'.$photo->getUrl().''
             );
 
-            dd($response);
+            dd($response->getContent());
 
            // $entityManager->persist($photo);
             //$entityManager->flush();
