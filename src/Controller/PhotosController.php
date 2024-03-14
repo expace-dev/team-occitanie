831dfe1fd808e56676c9985678ba8a560010f0a0
Fredy <fredy6786@gmail.com>
2024-03-14 11:14:26 +0100

    partager une photo posté

--- src/Controller/PhotosController.php
+++ src/Controller/PhotosController.php
@@ -137,6 +137,8 @@ class PhotosController extends AbstractController
                 'https://bot.team-occitanie.fr/post-photo/query/?username='.$photo->getUsers()->getUsername().'&avatar=https://www.team-occitanie.fr'.$photo->getUsers()->getAvatar().'&image=https://www.team-occitanie.fr'.$photo->getUrl().''
             );
 
+            dd($response);
+
            // $entityManager->persist($photo);
             //$entityManager->flush();
 
