d8c83c0484ad3c521a95c8745420341b7ab05e6e
Fredy <fredy6786@gmail.com>
2024-03-18 13:29:16 +0100

    editer un evenement

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -262,7 +262,7 @@ class ApplicationController extends AbstractController
 
             $response2 = $httpClient->request(
                 'GET',
-                'https://bot.team-occitanie.fr/add-evenement/query/?username='.$evenement->getAuteur()->getUsername().'&date='.$timestamp.'&description='.$evenement->getDescription().'&avatar='.$evenement->getAuteur()->getAvatar().'&type='.$evenement->getTypeSession().'&image=https://www.team-occitanie.fr'.$evenement->getVisuel().'&id='.$evenement->getDiscordId().'',
+                'https://bot.team-occitanie.fr/edit-evenement/query/?username='.$evenement->getAuteur()->getUsername().'&date='.$timestamp.'&description='.$evenement->getDescription().'&avatar='.$evenement->getAuteur()->getAvatar().'&type='.$evenement->getTypeSession().'&image=https://www.team-occitanie.fr'.$evenement->getVisuel().'&id='.$evenement->getDiscordId().'',
                 [
                     'headers' => [
                         'Origin' => 'https://www.team-occitanie.fr'
